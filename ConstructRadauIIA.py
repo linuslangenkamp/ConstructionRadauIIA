721cd9bc670c6c4846ac2d1cb1b07f6c25ebfa6b
linuslangenkamp <linus@DESKTOP-I2LHO59>
2024-10-09 00:54:53 +0200

    Added integral approach, seems to be about 10-100x faster

--- ConstructRadauIIA.py
+++ ConstructRadauIIA.py
@@ -124,6 +124,70 @@ def genCppCode(s, dps, mindps):
     print(outStr)
     return True
 
+
+### new integral approach
+
+def fixRoots(roots, dps=150):
+    allRoots = []
+    for r in roots:
+        rDps = N(r, dps)
+        if rDps.is_real:
+            allRoots.append(rDps)
+        elif abs(im(rDps)) <= 1e-150:
+            allRoots.append(re(rDps))
+        else:
+            rDps = N(r, dps)
+            allRoots.append(re(rDps))
+
+    allRoots.sort()
+    return allRoots
+
+x = symbols('x')
+
+def lagrange(c, j, dps=150):
+    p = mpf(1)
+    for i in range(len(c)):
+        if i != j:
+            p *= (x - c[i]) / (c[j] - c[i])
+    return [mpf(coeff) for coeff in Poly(expand(p)).all_coeffs()[::-1]]
+
+def integrate(poly, roots, i, dps=150):
+    S = mpf(0)
+    for j in range(len(roots)):
+        S += N(1, dps) / N(j+1, dps) * poly[j] * pow(roots[i], N(j+1, dps))
+    return S
+
+def generateFromIntegral(s, dps=150): 
+    mp.dps = dps
+
+    # calculate coeffs of d^(s-1) / dx^(s-1) x**(s-1) * (x-1)**s
+    function = x**(s-1) * (x-1)**s
+    der = expand(diff(function, x, s-1))
+    pDer = Poly(der, x)
+    roots = solve(pDer, x)
+
+    roots = fixRoots(roots)
+
+    c = [mpf(N(r, dps)) for r in roots]
+    
+    A = [[] for _ in range(len(c))]
+
+    for j in range(len(c)):
+        lagr = lagrange(c, j)
+        for i in range(len(c)):
+            a = integrate(lagr, c, i)
+            A[i].append(a)
+    return A, inverse(A), c
+
+import time
+
+for m in range(2, 50):
+    startTime = time.time()
+    A, Ainv, c = generateFromIntegral(m)
+    print(f"{m} {time.time() - startTime}")
+"""
 for s in range(1, 37):
     genCppCode(s, 150, 53)
     print("")
+"""
+
